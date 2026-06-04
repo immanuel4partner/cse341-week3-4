4c541f312deedabc3b9289ef2643ee613940be9d
Immanuel <immanuel4partner@gmai.com>
2026-06-04 14:02:10 +0100

    auth undefined

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,11 +1,8 @@
 const isAuthenticated = (req, res, next) => {
-  if (req.session && req.session.user) {
-    return next();
+  if (req.session.user === undefined) {
+    return res.status(401).json({ error: "You do not have permission to access this resource" });
   }
-
-  return res.status(401).json({
-    error: "You do not have permission to access this resource",
-  });
+  next();
 };
 
 module.exports = isAuthenticated;
